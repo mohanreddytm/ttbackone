a3ed29f5c7fe90f4a9622917bcf95144bf6f977f
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-07-08 17:29:41 +0530

    updated package json two

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const jwt = require('jsonwebtoken');
 
